57c59e289a55a5c8c2309fe390cb97f5e10bf158
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-08-16 10:40:37 -0700

    did a bit of refractoring

--- part-3/index.js
+++ part-3/index.js
@@ -1,41 +1,34 @@
-const book = document.getElementsByClassName('book');
-const modal = document.getElementById('booking');
-const close = document.getElementsByClassName('close')[0];
-const modalRoomNum = document.getElementById('roomNum');
-const modalPrice = document.getElementById('pricePerNight');
-const modalTotal = document.getElementById('total');
-const quantity = document.querySelector('#quantity');
+const book = document.querySelectorAll('.book')
+const modal = document.querySelector('#booking')
+const close = document.querySelectorAll('.close')[0]
+const modalRoomNumber = document.querySelector('#roomNumber')
+const modalPrice = document.querySelector('#pricePerNight')
+const modalTotal = document.querySelector('#total')
+const quantity = document.querySelector('#quantity')
 
-let room = [];
+let room = []
 
-for (var i = 0; i < book.length; i++) {
-	let b = book[i];
-	b.addEventListener('click', () => {
-		room.num = b.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
-		room.capacity = b.parentNode.previousElementSibling.previousElementSibling.innerHTML;
-		room.price = b.parentNode.previousElementSibling.innerHTML;
-		bookingContents();
-	});
+const bookingContents = () => {
+	modalRoomNumber.innerHTML = 'Room ' + room.num
+	modalPrice.innerHTML = room.price + '/ Night'
+	modal.style.display = 'block'
 }
 
-
-
-//MODAL
-//MODAL: CLOSE
-//will 'close' the Modal
-
-function bookingContents() {
-	modalRoomNum.innerHTML = 'Room ' + room.num;
-	modalPrice.innerHTML = room.price + '/ Night';
-	modal.style.display = 'block';
+for (let b of book) {
+	b.addEventListener('click', () => {
+		room.num = b.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML
+		room.capacity = b.parentNode.previousElementSibling.previousElementSibling.innerHTML
+		room.price = b.parentNode.previousElementSibling.innerHTML
+		bookingContents()
+	})
 }
 
 close.addEventListener('click', () => {
-	modal.style.display = 'none';
+	modal.style.display = 'none'
 })
 
 quantity.addEventListener('change', function (e) {
-	e.preventDefault();
-	let total = this.value * parseFloat(room.price.slice(1));
-	modalTotal.innerHTML = '$ ' + total.toFixed(2);
-});
+	e.preventDefault()
+	let total = this.value * parseFloat(room.price.slice(1))
+	modalTotal.innerHTML = '$ ' + total.toFixed(2)
+})
